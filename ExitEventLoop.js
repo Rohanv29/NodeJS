632ee5bf8ac3e195c4d36a8e26bbe898200f5886
2k25csaiml2513533-gif <2k25aiml2513533@gmail.com>
2026-08-15 00:30:40 +0530

    ExitEventLoopAndReqObj

---
+++ ExitEventLoop.js
@@ -0,0 +1,13 @@
+// Simple Node.js server
+const { log } = require('console');
+const http=require('http');
+
+const server=http.createServer((req,res)=>{
+    console.log(req.url,req.method,req.headers);
+    process.exit(); //stops event loop
+});
+const PORT=3000;
+server.listen(PORT,()=>{
+    console.log(`Server  running at http://localhost:${PORT}`);
+    
+})
