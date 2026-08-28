f415999d74029807304873cc9f3571950997e329
2k25csaiml2513533-gif <2k25aiml2513533@gmail.com>
2026-08-29 00:10:44 +0530

    server request handling

---
+++ ParsingRequest_04/server.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+const userRequestHandler = require('./user');
+
+const server = http.createServer(userRequestHandler);
+
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000');
+});
